a4d9306c18f383b7d3b62fd597333f1e95dd7eda
pellelito <pelle.lofstrand@gmail.com>
2020-09-15 10:05:32 +0200

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
       },
       {
         id: 3,
-        title: "Dinner with mistress",
+        title: "Dinner with friends",
         completed: false,
       },
     ],
